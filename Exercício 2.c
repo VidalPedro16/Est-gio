02bd11e55d83f6ebc00a120d483907226e4b47f7
VidalPedro16 <147095720+VidalPedro16@users.noreply.github.com>
2024-10-09 21:40:55 -0300

    Update Exercício 2.c

--- Exercício 2.c
+++ Exercício 2.c
@@ -5,18 +5,15 @@ int main() {
     char str[100];
     int i, count = 0;
     
-    // Solicitar ao usuário que insira uma string
     printf("Digite uma string: ");
     fgets(str, sizeof(str), stdin);
 
-    // Percorrer a string e verificar a presença de 'a' ou 'A'
     for (i = 0; str[i] != '\0'; i++) {
         if (str[i] == 'a' || str[i] == 'A') {
             count++;
         }
     }
 
-    // Exibir o resultado
     if (count > 0) {
         printf("A letra 'a' ou 'A' ocorre %d vez(es) na string.\n", count);
     } else {
